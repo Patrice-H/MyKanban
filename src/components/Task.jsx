708f08070248218140f81a526f97fba0f6e094aa
Patrice-H <webmestre64@gmail.com>
2022-07-22 12:18:28 +0200

    Add a functionality to delete a task

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,19 +1,64 @@
 import { Draggable } from 'react-beautiful-dnd';
 
 const Task = (props) => {
+  const setDashboard = props.setDashboard;
+  const setEditedTask = props.setEditedTask;
+  const setInputEntry = props.setInputEntry;
   const taskClass = 'task-item';
+
+  const deleteTask = (taskId) => {
+    const newTasksList = {
+      ...props.dashboard.tasks,
+    };
+    delete newTasksList[taskId];
+
+    const newTaskListIds = props.column.taskIds.filter((id) => id !== taskId);
+
+    const newColumns = {
+      ...props.dashboard.columns,
+      [props.column.id]: {
+        ...props.column,
+        taskIds: newTaskListIds,
+      },
+    };
+
+    const newDashboard = {
+      ...props.dashboard,
+      tasks: newTasksList,
+      columns: newColumns,
+    };
+
+    setDashboard(newDashboard);
+  };
 
   return (
     <Draggable draggableId={props.task.id} index={props.index}>
       {(provided, snapshot) => (
-        <p
+        <div
           {...provided.draggableProps}
           {...provided.dragHandleProps}
           ref={provided.innerRef}
           className={snapshot.isDragging ? taskClass + ' dragging' : taskClass}
         >
-          {props.task.content}
-        </p>
+          <h3>{props.task.content}</h3>
+          <div className="task-item-btns">
+            <span
+              className="fa-solid fa-pencil"
+              onClick={(e) => {
+                e.preventDefault();
+                setEditedTask(props.task.id);
+                setInputEntry(props.task.content);
+              }}
+            ></span>
+            <span
+              className="fa-solid fa-trash-can"
+              onClick={(e) => {
+                e.preventDefault();
+                deleteTask(props.task.id);
+              }}
+            ></span>
+          </div>
+        </div>
       )}
     </Draggable>
   );
