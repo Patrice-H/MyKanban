c48a4cf4b2764cc50b75879ccb1f5564fa019caa
Patrice-H <webmestre64@gmail.com>
2022-08-29 00:06:49 +0200

    Adding title h1 mui component

---
+++ src/components/Header/Heading1/index.jsx
@@ -0,0 +1,19 @@
+import { Typography } from '@mui/material';
+
+const Heading1 = () => {
+  return (
+    <Typography
+      variant="h1"
+      sx={{
+        fontSize: '36px',
+        fontWeight: 'bold',
+        textAlign: 'center',
+        margin: '20px',
+      }}
+    >
+      Mon Kanban
+    </Typography>
+  );
+};
+
+export default Heading1;
