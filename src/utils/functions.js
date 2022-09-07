86f7efa47a9c01da769540dbb52862b1c82f5d80
Patrice-H <webmestre64@gmail.com>
2022-09-07 16:36:30 +0200

    Adding a function to get column name from its id

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -12,3 +12,16 @@ export const convertTasksList = (list) => {
 
   return tasks;
 };
+
+export const getColumnName = (column) => {
+  switch (column) {
+    case 'column-1':
+      return 'to do';
+    case 'column-2':
+      return 'in progress';
+    case 'column-3':
+      return 'done';
+    default:
+      break;
+  }
+};
