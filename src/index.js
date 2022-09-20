bb8e7fcfd78eb64bae72c0951dda767893419a8c
Patrice-H <webmestre64@gmail.com>
2022-09-21 00:25:30 +0200

    Adding router logic

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 import './index.css';
-import App from './App';
+//import App from './App';
+import Dashboard from './pages/Dashboard';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/dashboard/:dashboardId" element={<Dashboard />}></Route>
+    </Routes>
+  </Router>
+);
