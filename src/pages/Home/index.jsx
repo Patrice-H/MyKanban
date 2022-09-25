8245e7d7adbe31cd77e31d167bd612990cca78e5
Patrice-H <webmestre64@gmail.com>
2022-09-25 21:13:28 +0200

    Insert dashboard modal

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,17 @@
 import { useState, useEffect } from 'react';
+import { Button } from '@mui/material';
 import Thumbnail from '../../components/Thumbnail';
 import { getDashboardsList } from '../../services/dbManager';
+import DashboardModal from '../../components/DashboardModal';
+import './Home.css';
 
 const Home = () => {
   const [pages, setPages] = useState();
+
+  const openDashboardModal = () => {
+    const modal = document.getElementById('dashboard-modal');
+    modal.classList.remove('hidden');
+  };
 
   useEffect(() => {
     let dashboardIds = [];
@@ -11,15 +19,30 @@ const Home = () => {
       data.data.forEach((item) => dashboardIds.push(item.id));
       setPages(dashboardIds);
     });
-  }, []);
+  }, [pages]);
 
   return (
     <div>
+      <Button
+        variant="contained"
+        id="add-dashboard-btn"
+        onClick={(e) => {
+          e.preventDefault();
+          openDashboardModal();
+        }}
+      >
+        Nouveau tableau
+      </Button>
       <h1 className="main-title">Tableaux de bord</h1>
       {pages &&
         pages.map((page) => (
-          <Thumbnail key={`thumbnail-${page}`} pageId={page} />
+          <Thumbnail
+            key={`thumbnail-${page}`}
+            pageId={page}
+            className="thumbnail"
+          />
         ))}
+      <DashboardModal />
     </div>
   );
 };
