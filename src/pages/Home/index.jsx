e2f27e6b6b464c3db0ebb638889bb823d98b11a7
Patrice-H <webmestre64@gmail.com>
2022-09-25 17:11:40 +0200

    Displaying all thumbnails from dashboard table database

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,14 +1,25 @@
+import { useState, useEffect } from 'react';
 import Thumbnail from '../../components/Thumbnail';
+import { getDashboardsList } from '../../services/dbManager';
 
 const Home = () => {
-  const pages = [1];
+  const [pages, setPages] = useState();
+
+  useEffect(() => {
+    let dashboardIds = [];
+    getDashboardsList().then((data) => {
+      data.data.forEach((item) => dashboardIds.push(item.id));
+      setPages(dashboardIds);
+    });
+  }, []);
 
   return (
     <div>
       <h1 className="main-title">Tableaux de bord</h1>
-      {pages.map((page) => (
-        <Thumbnail key={`thumbnail-${page}`} pageId={page} />
-      ))}
+      {pages &&
+        pages.map((page) => (
+          <Thumbnail key={`thumbnail-${page}`} pageId={page} />
+        ))}
     </div>
   );
 };
