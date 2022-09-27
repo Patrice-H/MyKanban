0c05012000b50014d64015311480bcb426383e0e
Patrice-H <webmestre64@gmail.com>
2022-09-27 11:49:38 +0200

    Modification of the initial data

--- src/data/initialData.js
+++ src/data/initialData.js
@@ -1,21 +1,5 @@
 export const initialData = {
   tasks: {},
-  columns: {
-    'column-1': {
-      id: 'column-1',
-      title: 'A faire',
-      taskIds: [],
-    },
-    'column-2': {
-      id: 'column-2',
-      title: 'En cours',
-      taskIds: [],
-    },
-    'column-3': {
-      id: 'column-3',
-      title: 'Fait',
-      taskIds: [],
-    },
-  },
-  columnOrder: ['column-1', 'column-2', 'column-3'],
+  columns: {},
+  columnOrder: [],
 };
