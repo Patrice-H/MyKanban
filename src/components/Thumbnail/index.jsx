17ef04aac7e15b3ca9a6fa64e55059b79fb6500d
Patrice-H <webmestre64@gmail.com>
2022-09-29 21:15:11 +0200

    Adding customized menu to the thumbnails

--- src/components/Thumbnail/index.jsx
+++ src/components/Thumbnail/index.jsx
@@ -1,18 +1,25 @@
 import { Link } from 'react-router-dom';
+import CustomizedMenu from '../CustomizedMenu';
 import './Thumbnail.css';
 
 const Thumbnail = (props) => {
   return (
-    <Link to={`/dashboard/${props.dashboardId}`} className="thumbnail">
-      <div className="mask"></div>
-      <iframe
-        title={`dashboard-${props.dashboardId}`}
-        width="1200"
-        height="500"
-        src={`http://localhost:3000/dashboard/${props.dashboardId}`}
-        scrolling="no"
-      ></iframe>
-    </Link>
+    <div className="thumbnail">
+      <Link
+        to={`/dashboard/${props.dashboardId}`}
+        className="thumbnail-dashboard"
+      >
+        <div className="mask"></div>
+        <iframe
+          title={`dashboard-${props.dashboardId}`}
+          width="1200"
+          height="500"
+          src={`http://localhost:3000/dashboard/${props.dashboardId}`}
+          scrolling="no"
+        ></iframe>
+      </Link>
+      <CustomizedMenu />
+    </div>
   );
 };
 
