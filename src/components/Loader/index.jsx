cf22bc6715fe520988238133e1d9454299c16be1
Patrice-H <webmestre64@gmail.com>
2022-10-04 08:43:08 +0200

    Adding a loader component

---
+++ src/components/Loader/index.jsx
@@ -0,0 +1,13 @@
+import './Loader.css';
+
+const Loader = () => {
+  return (
+    <div id="loader-container">
+      <div id="bar-1" className="bar"></div>
+      <div id="bar-2" className="bar"></div>
+      <div id="bar-3" className="bar"></div>
+    </div>
+  );
+};
+
+export default Loader;
