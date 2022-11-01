0fdb8e39df2082bce146a765e10e79cdd1f1c107
Patrice-H <webmestre64@gmail.com>
2022-11-01 21:50:07 +0100

    Add JsDoc to the dbManager functions

--- src/services/dbManager.js
+++ src/services/dbManager.js
@@ -1,11 +1,29 @@
 import { apiBaseUrl } from '../utils/config';
 
+/**
+ * Function to get the tasks list
+ *
+ * @description It returns a promise that resolves to a JSON object containing the response from the API.
+ * @returns {Promise<JSON>} The tasks list and a message
+ */
 export const getTasksList = async () => {
   return fetch(`${apiBaseUrl}/api/tasks`).then((response) => {
     return response.json();
   });
 };
 
+/**
+ * Function to create a task
+ *
+ * @description It takes in 5 arguments, creates a task with them, sends that object to the
+ * API, and returns a promise that resolves to a JSON object containing the response from the API.
+ * @param {string} title
+ * @param {string} description
+ * @param {number} order
+ * @param {number} category_id
+ * @param {number} dashboard_id
+ * @returns {Promise<JSON>} The created task and a message
+ */
 export const createTask = async (
   title,
   description,
@@ -33,6 +51,19 @@ export const createTask = async (
   });
 };
 
+/**
+ * Function to update a task
+ *
+ * @description It takes in 6 arguments, updates the task with them, sends that object to the
+ * API, and returns a promise that resolves to a JSON object containing the response from the API.
+ * @param {number} id
+ * @param {string} title
+ * @param {string} description
+ * @param {number} order
+ * @param {number} category_id
+ * @param {number} dashboard_id
+ * @returns {Promise<JSON>} The updated task and a message
+ */
 export const updateTask = async (
   id,
   title,
@@ -61,6 +92,14 @@ export const updateTask = async (
   });
 };
 
+/**
+ * Function to delete a task
+ *
+ * @description It takes an id as argument, makes a DELETE request to the API, and then it returns
+ * a promise that resolves to a JSON object containing the response from the API.
+ * @param {number} id
+ * @returns {Promise<JSON>} The deleted task and a message
+ */
 export const removeTask = async (id) => {
   const options = {
     method: 'DELETE',
@@ -73,12 +112,29 @@ export const removeTask = async (id) => {
   });
 };
 
+/**
+ * Function to get the categories list
+ *
+ * @description It returns a promise that resolves to a JSON object containing the response from the API.
+ * @returns {Promise<JSON>} The categories list and a message
+ */
 export const getCategoriesList = async () => {
   return fetch(`${apiBaseUrl}/api/categories`).then((response) => {
     return response.json();
   });
 };
 
+/**
+ * Function to create a category
+ *
+ * @description It takes in 4 arguments, creates a category with them, sends that object to the
+ * API, and returns a promise that resolves to a JSON object containing the response from the API.
+ * @param {string} title
+ * @param {number} order
+ * @param {number} background_color
+ * @param {number} dashboard_id
+ * @returns {Promise<JSON>} The created category and a message
+ */
 export const createCategory = async (
   title,
   order,
@@ -104,6 +160,18 @@ export const createCategory = async (
   });
 };
 
+/**
+ * Function to update a category
+ *
+ * @description It takes in 5 arguments, updates the category with them, sends that object to the
+ * API, and returns a promise that resolves to a JSON object containing the response from the API.
+ * @param {number} id
+ * @param {string} title
+ * @param {number} order
+ * @param {number} background_color
+ * @param {number} dashboard_id
+ * @returns {Promise<JSON>} The updated category and a message
+ */
 export const updateCategory = async (
   id,
   title,
@@ -130,6 +198,14 @@ export const updateCategory = async (
   });
 };
 
+/**
+ * Function to delete a category
+ *
+ * @description It takes an id as argument, makes a DELETE request to the API, and then it returns
+ * a promise that resolves to a JSON object containing the response from the API.
+ * @param {number} id
+ * @returns {Promise<JSON>} The deleted category and a message
+ */
 export const removeCategory = async (id) => {
   const options = {
     method: 'DELETE',
@@ -142,18 +218,40 @@ export const removeCategory = async (id) => {
   });
 };
 
+/**
+ * Function to get the dashboards list
+ *
+ * @description It returns a promise that resolves to a JSON object containing the response from the API.
+ * @returns {Promise<JSON>} The dashboards list and a message
+ */
 export const getDashboardsList = async () => {
   return fetch(`${apiBaseUrl}/api/dashboards`).then((response) => {
     return response.json();
   });
 };
 
+/**
+ * Function to get a dashboard
+ *
+ * @description It takes an id as argument, and then it returns
+ * a promise that resolves to a JSON object containing the response from the API.
+ * @param {number} id
+ * @returns {Promise<JSON>} The dashboard and a message
+ */
 export const getDashboard = async (id) => {
   return fetch(`${apiBaseUrl}/api/dashboard/${id}`).then((response) => {
     return response.json();
   });
 };
 
+/**
+ * Function to create a dashboard
+ *
+ * @description It takes a title as argument, creates a dashboard with it, sends that object to the
+ * API, and returns a promise that resolves to a JSON object containing the response from the API.
+ * @param {string} title
+ * @returns {Promise<JSON>} The created dashboard and a message
+ */
 export const createDashboard = async (title) => {
   const newDashboard = {
     title,
@@ -171,6 +269,15 @@ export const createDashboard = async (title) => {
   });
 };
 
+/**
+ * Function to update a dashboard
+ *
+ * @description It takes an id and a title as arguments, updates the dashboard with them, sends that object to the
+ * API, and returns a promise that resolves to a JSON object containing the response from the API.
+ * @param {number} id
+ * @param {string} title
+ * @returns {Promise<JSON>} The updated dashboard and a message
+ */
 export const updateDashboard = async (id, title) => {
   const updatedTask = {
     title,
@@ -190,6 +297,14 @@ export const updateDashboard = async (id, title) => {
   );
 };
 
+/**
+ * Function to delete a dashboard
+ *
+ * @description It takes an id as argument, makes a DELETE request to the API, and then it returns
+ * a promise that resolves to a JSON object containing the response from the API.
+ * @param {number} id
+ * @returns {Promise<JSON>} The deleted dashboard and a message
+ */
 export const removeDashboard = async (id) => {
   const options = {
     method: 'DELETE',
